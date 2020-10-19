8f1f0645e8dac8bb596b6793a3d007b99f5d3fa8
Simon Struck <simon@simonscode.org>
2020-10-19 13:19:19 +0200

    Improve build script

--- build_push_run.sh
+++ build_push_run.sh
@@ -1,2 +1,23 @@
-cross build --target armv7-unknown-linux-gnueabihf --release && ssh remarkable "killall pipes-and-rust" && scp ./target/armv7-unknown-linux-gnueabihf/release/pipes-and-rust remarkable: && ssh remarkable "./pipes-and-rust"
+#!/usr/bin/env zsh
 
+version="$1"
+host="remarkable"
+arch="armv7-unknown-linux-musleabihf"
+#arch="armv7-unknown-linux-gnueabihf"
+
+echo "Compiling..."
+if [ "$version" = "release" ]; then
+  cross build --target "$arch" --release
+else
+  cross build --target "$arch"
+fi
+echo "Done"
+
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+  echo "Killing last process..."
+  ssh remarkable "killall pipes-and-rust"
+  echo "Done"
+  echo "Copying to device..."
+  scp "./target/$arch/$version/pipes-and-rust" "$host:" && echo "Done" && ssh "$host" "./pipes-and-rust"
+fi
