802d8e4e123c6eda3e8c12e49288cda9bee7a7c1
Simon Struck <simon@simonscode.org>
2020-12-17 23:59:18 +0100

    WIP: Small UI improvements

--- build_push_run.sh
+++ build_push_run.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env zsh
 
-build_type="$1"
 host="remarkable"
 arch="armv7-unknown-linux-musleabihf"
 #arch="armv7-unknown-linux-gnueabihf"
 
-echo "Compiling..."
-if [ "$build_type" = "release" ]; then
-  cross build --target "$arch" --release
-else
-  cross build --target "$arch"
+function try_or_fail() {
+  # shellcheck disable=SC2091
+  if eval "$1"; then
+    echo "Done"
+  else
+    echo "Error"
+    exit 1
+  fi
+}
+
+
+echo "Checking if Docker is running..."
+if ! systemctl status docker.service > /dev/null; then
+  echo "Done"
+  echo "Starting Docker..."
+  sudo systemctl start docker.service
 fi
 echo "Done"
 
-# shellcheck disable=SC2181
-if [ $? -eq 0 ]; then
-  echo "Killing last process..."
-  ssh remarkable "killall pipes-and-rust"
-  echo "Done"
-  echo "Copying to device..."
-  scp "./target/$arch/$build_type/pipes-and-rust" "$host:/opt/pipes-and-rust" && echo "Done" && ssh "$host" "/opt/pipes-and-rust"
-fi
+echo "Compiling..."
+try_or_fail "cross build --target $arch --release"
+
+echo "Killing last process..."
+ssh "$host" "killall pipes-and-rust"
+echo "Done"
+
+echo "Checking if /opt/ directory exists in device..."
+try_or_fail "ssh $host 'mkdir -p /opt/'"
+
+echo "Copying to device..."
+try_or_fail "scp \"./target/$arch/release/pipes-and-rust\" \"$host:/opt/pipes-and-rust\""
+
+echo "Running..."
+try_or_fail "ssh $host \"/opt/pipes-and-rust\""
